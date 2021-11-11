2374260a221c2f0a4c0cd40494ef359ff3184fec
Kevin Lanckriet <kevin.lanckriet@student.ehb.be>
2021-11-11 14:38:35 +0100

    feat(GETs): GET tests written

--- __tests__/helper.test.js
+++ __tests__/helper.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { response } = require('../app');
 const app = require('../app');
 
 it('Test the test setup', () => {});
@@ -13,15 +14,50 @@ describe('Tests for GET functionalities', () => {
                 expect(response.body).toEqual(expect.arrayContaining([
                     expect.objectContaining({
                         userid: expect.any(Int)
-                        
                     })
                 ]))
             });
     });
-    it('GET /posts/userid returns the posts of a specific user', () => {});
-    it('GET /posts/userid when invalid id return 404', () => {});
-    it('GET /weatherid return an array', () => {});
-    it('GET /feelingid returns an array', () => {});
+    it('GET /posts/userid returns the posts of a specific user', () => {
+        return request(app)
+            .get('/posts/1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual(expect.objectContaining([{
+                    userid: expect.any(Int),
+                    posts: expect.arrayContaining([
+                        expect.objectContaining({
+                            weather: expect.any(String),
+                            mood: expect.any(String)
+                        })
+                    ]) 
+                }]))
+            });
+    });
+    it('GET /posts/userid when invalid id return 404', () => {
+        return request(app).get('/posts/invalid').expect(404);
+    });
+    it('GET /weatherid return an array of moods', () => {
+        return request(app)
+            .get('/weather/1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual(
+                    expect.any(Array))
+            })
+    });
+    it('GET /feelingid returns an array', () => {
+        return request(app)
+            .get('/feeling/1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual(
+                    expect.any(Array))
+            })
+    });
 });
 
 describe('Tests for DELETE functionalities', () => {
